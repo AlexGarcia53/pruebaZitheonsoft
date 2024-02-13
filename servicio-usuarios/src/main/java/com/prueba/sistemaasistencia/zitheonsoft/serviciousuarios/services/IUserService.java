015917d9539b61d563e1bebc93ed00ef37c1a2e6
AlexGarcia53 <alex.garcia228721@potros.itson.edu.mx>
2024-02-13 12:19:30 -0700

    avance en actualizar y eliminar
    controller y service actualizados

--- servicio-usuarios/src/main/java/com/prueba/sistemaasistencia/zitheonsoft/serviciousuarios/services/IUserService.java
+++ servicio-usuarios/src/main/java/com/prueba/sistemaasistencia/zitheonsoft/serviciousuarios/services/IUserService.java
@@ -1,7 +1,16 @@
 package com.prueba.sistemaasistencia.zitheonsoft.serviciousuarios.services;
 
+import java.util.Optional;
+
 import com.prueba.sistemaasistencia.zitheonsoft.serviciousuarios.entities.User;
 
 public interface IUserService {
     User save(User user);
+
+    Optional<User> findById(Long id);
+
+    Optional<User> update(Long id, User user);
+
+    Optional<User> delete(Long id);
+
 }
--- servicio-usuarios/src/main/java/com/prueba/sistemaasistencia/zitheonsoft/serviciousuarios/services/UserService.java
+++ servicio-usuarios/src/main/java/com/prueba/sistemaasistencia/zitheonsoft/serviciousuarios/services/UserService.java
@@ -1,5 +1,7 @@
 package com.prueba.sistemaasistencia.zitheonsoft.serviciousuarios.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,4 +20,47 @@ public class UserService implements IUserService{
     public User save(User user) {
         return userRepository.save(user);
     }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
+
+    @Override
+    public Optional<User> update(Long id, User newUser) {
+        Optional<User> userOptional = userRepository.findById(id);
+        if (userOptional.isPresent()) {
+            User userObtained = userOptional.orElseThrow();
+
+            userObtained.setName(newUser.getName());
+            userObtained.setLastname(newUser.getLastname());
+            userObtained.setBirthDate(newUser.getBirthDate());
+            userObtained.setGender(newUser.getGender());
+            userObtained.setCivilStatus(newUser.getCivilStatus());
+            userObtained.setNacionality(newUser.getNacionality());
+            userObtained.setAddress(newUser.getAddress());
+            userObtained.setContactData(newUser.getContactData());
+            userObtained.setLaborData(newUser.getLaborData());
+            userObtained.setEmergencyContacts(newUser.getEmergencyContacts());
+            userObtained.setAcademicDegrees(newUser.getAcademicDegrees());
+            userObtained.setLanguages(newUser.getLanguages());
+            userObtained.setCourses(newUser.getCourses());
+            return Optional.of(userRepository.save(userObtained));
+        }
+        
+        return userOptional;
+        
+    }
+
+    @Transactional
+    @Override
+    public Optional<User> delete(Long id) {
+        Optional<User> userOptional = userRepository.findById(id);
+        userOptional.ifPresent(userObtained -> {
+            userRepository.delete(userObtained);
+        });
+        return userOptional;
+    }
+
 }
